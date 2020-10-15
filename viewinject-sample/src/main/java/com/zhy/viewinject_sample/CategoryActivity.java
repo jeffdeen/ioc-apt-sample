c9c8048a29c9736bb3604cfa4516e84aaad7fc1a
jefferyding <jefferyding@tencent.com>
2020-10-15 11:53:09 +0800

    修改支持androidx

---
+++ viewinject-sample/src/main/java/com/zhy/viewinject_sample/CategoryActivity.java
@@ -0,0 +1,54 @@
+package com.zhy.viewinject_sample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zhy.ioc.Bind;
+import com.zhy.ioc.ViewInjector;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Created by zhy on 16/4/12.
+ */
+public class CategoryActivity extends AppCompatActivity
+{
+
+    @Bind(R.id.id_category_listview)
+    ListView mCategoryLv;
+
+    private List<String> mData = new ArrayList<>(Arrays.asList("Simple Use", "RecyclerView Use"));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        ViewInjector.injectView(this);
+
+        mCategoryLv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                categoryItemClick(position);
+            }
+        });
+
+        mCategoryLv.setAdapter(new CategoryAdapter(this, mData));
+    }
+
+    public void categoryItemClick(int position)
+    {
+        Toast.makeText(CategoryActivity.this, "position = " + position, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ viewinject-sample/src/main/java/com/zhy/viewinject_sample/SimpleFragment.java
@@ -0,0 +1,41 @@
+package com.zhy.viewinject_sample;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.zhy.ioc.Bind;
+import com.zhy.ioc.ViewInjector;
+
+/**
+ * Created by zhy on 15/1/1.
+ */
+public class SimpleFragment extends Fragment
+{
+    @Bind(R.id.id_textview)
+    TextView mIdTextview;
+    @Bind(R.id.id_btn)
+    Button mIdBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        ViewInjector.injectView(this, view);
+
+        mIdTextview.setText("ViewInject");
+        mIdBtn.setText("ViewInject ~");
+
+        return view;
+    }
+
+
+}
